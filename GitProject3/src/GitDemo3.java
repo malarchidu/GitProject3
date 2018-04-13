7faa201504a180c7b83a352f14aeca40606561b1
malar <malar@LAPTOP-00QR25KJ>
2018-04-13 10:33:25 -0700

    new code

--- GitProject3/src/GitDemo3.java
+++ GitProject3/src/GitDemo3.java
@@ -4,6 +4,7 @@ public class GitDemo3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Hello Solano");
+       System.out.println("county");
 	}
 
 }
